d03e75e607643d35894381ab90dc2cc067644484
Evgenii Shcherbakov <iipekolict@gmail.com>
2022-10-09 03:47:04 +0300

    refactor: separate config classes & plugin file

--- framework/src/main/kotlin/com/github/iipekolict/knest/KNestPlugin.kt
+++ framework/src/main/kotlin/com/github/iipekolict/knest/KNestPlugin.kt
@@ -1,60 +1,15 @@
 package com.github.iipekolict.knest
 
 import com.github.iipekolict.knest.builders.ControllerBuilder
+import com.github.iipekolict.knest.data.KNestConfiguration
+import com.github.iipekolict.knest.data.KNestConfigurationPublic
 import io.github.smiley4.ktorswaggerui.SwaggerUI
-import io.github.smiley4.ktorswaggerui.SwaggerUIPluginConfig
 import io.ktor.server.application.*
 import io.ktor.server.application.hooks.*
 import io.ktor.server.plugins.callloging.*
 import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.plugins.cors.*
 import io.ktor.server.plugins.cors.routing.CORS
 import io.ktor.server.routing.*
-
-class KNestConfiguration(
-    val controllers: Set<Any>,
-    val corsConfiguration: (CORSConfig.() -> Unit)?,
-    val callLoggingConfiguration: (CallLoggingConfig.() -> Unit),
-    val contentNegotiationConfiguration: (ContentNegotiationConfig.() -> Unit),
-    val swaggerConfiguration: (SwaggerUIPluginConfig.() -> Unit)
-)
-
-class KNestConfigurationPublic {
-    private var controllers: Set<Any> = emptySet()
-    private var corsConfiguration: (CORSConfig.() -> Unit)? = null
-    private var callLoggingConfiguration: (CallLoggingConfig.() -> Unit) = {}
-    private var contentNegotiationConfiguration: (ContentNegotiationConfig.() -> Unit) = {}
-    private var swaggerConfiguration: (SwaggerUIPluginConfig.() -> Unit) = {}
-
-    val configuration: KNestConfiguration
-        get() = KNestConfiguration(
-            controllers = controllers,
-            corsConfiguration = corsConfiguration,
-            callLoggingConfiguration = callLoggingConfiguration,
-            contentNegotiationConfiguration = contentNegotiationConfiguration,
-            swaggerConfiguration = swaggerConfiguration
-        )
-
-    fun setControllers(vararg varargControllers: Any) {
-        controllers = varargControllers.toSet()
-    }
-
-    fun cors(configuration: CORSConfig.() -> Unit) {
-        corsConfiguration = configuration
-    }
-
-    fun logging(configuration: CallLoggingConfig.() -> Unit) {
-        callLoggingConfiguration = configuration
-    }
-
-    fun contentNegotiation(configuration: ContentNegotiationConfig.() -> Unit) {
-        contentNegotiationConfiguration = configuration
-    }
-
-    fun swagger(configuration: SwaggerUIPluginConfig.() -> Unit) {
-        swaggerConfiguration = configuration
-    }
-}
 
 val KNest = createApplicationPlugin(
     name = "KNest",
---
+++ framework/src/main/kotlin/com/github/iipekolict/knest/data/KNestConfigurationPublic.kt
@@ -0,0 +1,43 @@
+package com.github.iipekolict.knest.data
+
+import io.github.smiley4.ktorswaggerui.SwaggerUIPluginConfig
+import io.ktor.server.plugins.callloging.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.cors.*
+
+class KNestConfigurationPublic {
+    private var controllers: Set<Any> = emptySet()
+    private var corsConfiguration: (CORSConfig.() -> Unit)? = null
+    private var callLoggingConfiguration: (CallLoggingConfig.() -> Unit) = {}
+    private var contentNegotiationConfiguration: (ContentNegotiationConfig.() -> Unit) = {}
+    private var swaggerConfiguration: (SwaggerUIPluginConfig.() -> Unit) = {}
+
+    val configuration: KNestConfiguration
+        get() = KNestConfiguration(
+            controllers = controllers,
+            corsConfiguration = corsConfiguration,
+            callLoggingConfiguration = callLoggingConfiguration,
+            contentNegotiationConfiguration = contentNegotiationConfiguration,
+            swaggerConfiguration = swaggerConfiguration
+        )
+
+    fun setControllers(vararg varargControllers: Any) {
+        controllers = varargControllers.toSet()
+    }
+
+    fun cors(configuration: CORSConfig.() -> Unit) {
+        corsConfiguration = configuration
+    }
+
+    fun logging(configuration: CallLoggingConfig.() -> Unit) {
+        callLoggingConfiguration = configuration
+    }
+
+    fun contentNegotiation(configuration: ContentNegotiationConfig.() -> Unit) {
+        contentNegotiationConfiguration = configuration
+    }
+
+    fun swagger(configuration: SwaggerUIPluginConfig.() -> Unit) {
+        swaggerConfiguration = configuration
+    }
+}

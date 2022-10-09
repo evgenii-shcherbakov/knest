1d3c9f411c9bbe00c647ca576420a1002672fba6
Evgenii Shcherbakov <iipekolict@gmail.com>
2022-10-09 18:49:15 +0300

    feat: separate deploy for submodules

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ val libraryVersion: String by project
 val libraryGroup: String by project
 
 plugins {
+    `java-library`
+    `maven-publish`
     kotlin("jvm") version "1.7.10"
 }
 
@@ -14,6 +16,63 @@ allprojects {
 
         maven {
             setUrl("https://jitpack.io")
+        }
+    }
+}
+
+subprojects {
+    apply(plugin = "java-library")
+    apply(plugin = "maven-publish")
+
+    java {
+        withJavadocJar()
+        withSourcesJar()
+    }
+
+    publishing {
+        publications {
+            create<MavenPublication>("mavenJava") {
+                groupId = project.group.toString()
+                artifactId = project.name
+                version = project.version.toString()
+                from(components["java"])
+
+                versionMapping {
+                    usage("java-api") {
+                        fromResolutionOf("runtimeClasspath")
+                    }
+
+                    usage("java-runtime") {
+                        fromResolutionResult()
+                    }
+                }
+
+                pom {
+                    name.set("KNest")
+                    description.set("Nest-like framework based on Ktor")
+
+                    licenses {
+                        license {
+                            name.set("MIT License")
+                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+
+                    developers {
+                        developer {
+                            id.set("iipekolict")
+                            name.set("Evgenii Shcherbakov")
+                            email.set("iipekolict@gmail.com")
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    tasks.javadoc {
+        if (JavaVersion.current().isJava9Compatible) {
+            (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
         }
     }
 }
--- framework/build.gradle.kts
+++ framework/build.gradle.kts
@@ -4,8 +4,6 @@ val logbackVersion: String by project
 val swaggerVersion: String by project
 
 plugins {
-    `java-library`
-    `maven-publish`
     kotlin("jvm") version "1.7.10"
 }
 
@@ -18,56 +16,4 @@ dependencies {
     implementation ("io.github.smiley4:ktor-swagger-ui:$swaggerVersion")
 
     implementation("io.ktor:ktor-server-call-logging:$ktorVersion")
-}
-
-java {
-    withJavadocJar()
-    withSourcesJar()
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            groupId = project.group.toString()
-            artifactId = project.name
-            version = project.version.toString()
-            from(components["java"])
-
-            versionMapping {
-                usage("java-api") {
-                    fromResolutionOf("runtimeClasspath")
-                }
-
-                usage("java-runtime") {
-                    fromResolutionResult()
-                }
-            }
-
-            pom {
-                name.set("KNest")
-                description.set("Nest-like framework based on Ktor")
-
-                licenses {
-                    license {
-                        name.set("MIT License")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-
-                developers {
-                    developer {
-                        id.set("iipekolict")
-                        name.set("Evgenii Shcherbakov")
-                        email.set("iipekolict@gmail.com")
-                    }
-                }
-            }
-        }
-    }
-}
-
-tasks.javadoc {
-    if (JavaVersion.current().isJava9Compatible) {
-        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
-    }
 }
